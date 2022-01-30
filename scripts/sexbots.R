54259b48e33c7b4f2ef2c1859effcad6814a8943
Jill Walker Rettberg <44365847+jilltxt@users.noreply.github.com>
2022-01-29 18:34:01 -0600

    Fixed visualisations of Döring data, but robably don't need it actually.

--- scripts/sexbots.R
+++ scripts/sexbots.R
@@ -28,19 +28,44 @@ sexbots <- structure(list(
 sexbots %>% 
         filter(Variables == "Female" | Variables == "Male") %>%
         ggplot() +
-        geom_col(aes(x = Variables, y = Human, fill = "Variables" )) +
-        geom_col(aes(x = Variables, y = Robot, fill = "Variables" )) 
+        geom_col(aes(x = Variables, y = Human, fill = Variables )) +
+        geom_col(aes(x = Variables, y = Robot, fill = Variables )) 
 
 # Here is another version of the sexbots dataframe that includes the ages as
 # well.
 
-sexbots_all <- structure(list(
+sexbots <- structure(list(
         Variables = c("Female", "Male", "Child", "Adult", "Elderly", 
                       "Heterosexual", "Homosexual", "Other"), 
         Human = c(43, 62, 27, 57, 2, 78, 21, 1), 
-        Robot = c(46, 55, 23, 55, NA, 78, 21, 1)), 
+        Robot = c(46, 55, 23, 55, NA, 78, 21, 1), 
+        Type = c("Gender", "Gender", "Age", "Age", "Age", "Sexuality", "Sexuality", "Sexuality")),
         row.names = c(NA, -8L), 
         class = c("tbl_df", "tbl", "data.frame"))
+
+sexbots %>% 
+        filter(Variables == "Female" | Variables == "Male") %>%
+        ggplot() +
+        geom_col(aes(x = Variables, y = Human, fill = Variables )) 
+
+ggplot(data = sexbots, group = Type) +
+        geom_col(mapping=aes(x = Variables,
+                             y = Human,
+                             fill = Type)) +
+        ggtitle("Characteristics of humans who had sexual relationships with robots") +
+        ylab("Percentage of sample") +
+        xlab("Traits") +
+        facet_wrap(~ Type, scale="free_x")
+        
+ggplot(data = sexbots, group = Type) +
+        geom_col(mapping=aes(x = Variables,
+                             y = Robot,
+                             fill = Type)) +
+        ggtitle("Characteristics of robots who had sexual relationships with humans") +
+        ylab("Percentage of sample") +
+        xlab("Traits") +
+        facet_wrap(~ Type, scale="free_x")
+
 
 # I emailed the authors of the paper (Jan 29, 2022) to ask if I could have their
 # full dataset - it would be cool if they have some of the same works in their data!
