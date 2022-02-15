6c83c2cc42ee84f43cf3ae8b830bd56701ed2652
Jill Walker Rettberg <44365847+jilltxt@users.noreply.github.com>
2022-02-15 17:06:23 -0600

    Script to import situations_long.csv, remove GenreID column and subgenres and save as situations.csv.

---
+++ scripts/clean_situations_long.R
@@ -0,0 +1,23 @@
+# Import situations_long.csv, remove unnecessary columns and rows with subgenres.
+# Then save as situations.csv.
+# 
+
+
+
+Situations-long <- read_csv("data/situations_long.csv",
+        col_types = cols(
+                SituationID = col_integer(),
+                Situation = col_skip(),
+                Genre = col_character(),
+                GenreID = col_skip(), 
+                Character = col_character(),
+                Entity = col_character(),
+                Technology = col_character(),
+                Verb = col_character()
+        )
+)
+
+situations_long <- situations_long %>%  
+        filter(Genre == "Art" | Genre == "Narrative" | Genre == "Game")
+
+write_csv(situations_long, "../data/situations.csv")
