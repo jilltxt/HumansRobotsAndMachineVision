b89ab9cd49a10e3111ad0733b15980317e01ec1c
Jill Walker Rettberg <44365847+jilltxt@users.noreply.github.com>
2022-04-14 06:53:27 -0500

    update

--- scripts/clean_situations_long.R
+++ scripts/clean_situations_long.R
@@ -2,22 +2,27 @@
 # Then save as situations.csv.
 # 
 
+#import the original export (misspellings and all)
 
+situations <- read_csv("data/sitiuations_long_v3.csv")
 
-Situations_long <- read_csv("data/situations_long.csv",
-        col_types = cols(
-                SituationID = col_integer(),
-                SituationTitle = col_skip(),
-                Genre = col_character(),
-                GenreID = col_skip(), 
-                Character = col_character(),
-                Entity = col_character(),
-                Technology = col_character(),
-                Verb = col_character()
-        )
-)
+situations[!rowSums(is.na(situations[-1:4, 8])),]
+# filter so it ONLY includes art, game, narrative and not duplicate lines for all the 
+# subgenres.
+# Also filter out the few rows which list a verb with no agent - I went through
+# the database looking for these and can't see why they're listed that way. So
+# will just delete them.
+# 
+# WHY WON'T THIS WORK!!!!
 
-situations_long <- situations_long %>%  
-        filter(Genre == "Art" | Genre == "Narrative" | Genre == "Game")
+situations %>%  
+        filter(Genre == "Art" | Genre == "Narrative" | Genre == "Game") %>% 
+        filter(!is.na(Technology) & !is.na(Entity))
+               
+               %>% 
+        select(SituationID, Situation = SituationTitle, Genre, Character, 
+               CharacterID, Entity, Technology, Verb)
 
-write_csv(situations_long, "data/situations.csv")
+write_csv(situations, "data/situations.csv")
+
+rownames(situations, is.na(Technology) && is.na(Entity) && is.na(Character))
